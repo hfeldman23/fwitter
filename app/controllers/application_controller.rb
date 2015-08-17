2bac5da157f22e72b549e1b7735b2a846fb907ee
Henry Feldman <hfeldman23@gmail.com>
2015-08-17 17:31:54 +0000

    basic structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require "./config/environment"
+
+class ApplicationController < Sinatra::Base
+  get "/" do
+    "Hello World!"
+  end
+  
+  configure do
+  set :public_folder, "public"
+  set :views, "app/views"
+  end
+end
